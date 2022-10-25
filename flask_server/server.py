3c490a8d02c8f6b25b4dfff12534deb3025f81f1
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-25 16:45:24 -0600

    Reading more about alchemy

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,64 +1,35 @@
 from flask import Flask
-from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
 # create the extension
 db = SQLAlchemy()
 # create the app
 app = Flask(__name__)
-CORS(app)
-
-# we can put high scores here, or really whatever we want to store
-# at any given route
-
-
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 # initialize the app with the extension
 db.init_app(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String)
 
 with app.app_context():
     db.create_all()
 
 @app.route("/users")
 def user_list():
-    users = db.session.execute(db.select(User).order_by(User.username)).scalars()
-    return render_template("user/list.html", users=users)
+    print("users data")
 
-@app.route("/users/create", methods=["GET", "POST"])
-def user_create():
-    if request.method == "POST":
-        user = User(
-            username=request.form["username"],
-            email=request.form["email"],
-        )
-        db.session.add(user)
-        db.session.commit()
-        return redirect(url_for("user_detail", id=user.id))
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(50))
+#     email = db.Column(db.String(100), unique=True)
+#     date_joined = db.Column(db.Date, default=datetime.utcnow)
 
-    return render_template("user/create.html")
+#     def __repr__(self):
+#         return f'<User: {self.email}>'
 
-@app.route("/user/<int:id>")
-def user_detail(id):
-    user = db.get_or_404(User, id)
-    return render_template("user/detail.html", user=user)
 
-@app.route("/user/<int:id>/delete", methods=["GET", "POST"])
-def user_delete(id):
-    user = db.get_or_404(User, id)
 
-    if request.method == "POST":
-        db.session.delete(user)
-        db.session.commit()
-        return redirect(url_for("user_list"))
-
-    return render_template("user/delete.html", user=user)
-
+# app = Flask(__name__)
+# CORS(app)
 
 # @app.route('/api/members')
 # def members():
@@ -75,5 +46,5 @@ def user_delete(id):
 # def random():
 #     return {"random": 42}
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
