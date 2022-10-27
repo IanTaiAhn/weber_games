5f19cf21cf37f5a9d4abb821224f8cf19a3fd1fd
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-26 23:13:15 -0600

    Finally we got past app_context() bug.

--- flask_server/app.py
+++ flask_server/app.py
@@ -2,14 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite3'
-
-db = SQLAlchemy(app)
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    UserName = db.Column(db.String(50))
-    UserPass = db.Column(db.String(50))
-    UserDisplayName = db.Column(db.String(50))
+with app.app_context():
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite3'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+    db = SQLAlchemy(app)
+    class User(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        UserName = db.Column(db.String(50))
+        UserPass = db.Column(db.String(50))
+        UserDisplayName = db.Column(db.String(50))
+    db.create_all()
