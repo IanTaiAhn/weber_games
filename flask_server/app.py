2ceb0b06ba2d0194b937ea526f1563642c67405e
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-28 16:18:34 -0600

    Making project file structure more robust and scalable.

--- flask_server/app.py
+++ flask_server/app.py
@@ -2,39 +2,43 @@ from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
-# from flask_cors import CORS
 
-app = Flask(__name__)
-basedir = os.path.abspath(os.path.dirname(__file__))
-# Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-# Init db
-db = SQLAlchemy(app)
-# Init ma... Helps with schema stuff?
-ma = Marshmallow(app)
+# Here I'm going to try to import the Schemas/tables, and see if that is an option to keep code cleaner.
+import tables.UserTable
+from tables.UserTable import app, db
+
+# app = Flask(__name__)
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# # Database
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+# # Init db
+# db = SQLAlchemy(app)
+# # Init ma... Helps with schema stuff?
+# ma = Marshmallow(app)
 
 
-# User Class/Model
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    UserName = db.Column(db.String(50))
-    UserPass = db.Column(db.String(50))
-    UserDisplayName = db.Column(db.String(50))
+# # User Class/Model
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     UserName = db.Column(db.String(50))
+#     UserPass = db.Column(db.String(50))
+#     UserDisplayName = db.Column(db.String(50))
 
-def __init__(self, UserName, UserPass, UserDisplayName):
-    self.UserName = UserName
-    self.UserPass = UserPass
-    self.UserDisplayName = UserDisplayName
+# def __init__(self, UserName, UserPass, UserDisplayName):
+#     self.UserName = UserName
+#     self.UserPass = UserPass
+#     self.UserDisplayName = UserDisplayName
 
-# User Schema
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'UserName', 'UserPass', 'UserDisplayName')
+# # User Schema
+# class UserSchema(ma.Schema):
+#     class Meta:
+#         fields = ('id', 'UserName', 'UserPass', 'UserDisplayName')
 
-# Init schema
-user_schema = UserSchema()
-users_schema = UserSchema()
+# # Init schema
+# user_schema = UserSchema()
+# users_schema = UserSchema()
+
 
 # API call to create a User
 @app.route('/add_user', methods=['POST'])
@@ -43,22 +47,21 @@ def create_user():
     UserPass = request.json['UserPass']
     UserDisplayName = request.json['UserDisplayName']
 
-    add_user = User(UserName=UserName, UserPass=UserPass, UserDisplayName=UserDisplayName)
+    add_user = tables.UserTable.User(UserName=UserName, UserPass=UserPass, UserDisplayName=UserDisplayName)
     db.session.add(add_user)
     db.session.commit()
 
-    return user_schema.jsonify(add_user)
+    return tables.UserTable.user_schema.jsonify(add_user)
 
 # Get a User
 @app.route('/user', methods=['GET'])
 def get_users():
-    all_users = User.query.get(1)
-    result = users_schema.dump(all_users)
+    all_users = tables.UserTable.User.query.get(1)
+    # result = tables.UserTable.users_schema.dump(all_users)
     # users = db.session.execute(db.select(User).order_by(User.UserName))
-    return users_schema.jsonify(all_users)
+    return tables.UserTable.users_schema.jsonify(all_users)
     # return jsonify(result)
     
-
 # Test API Call
 @app.route('/test')
 def test():
---
+++ flask_server/tables/UserTable.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+# from ..app import *
+# import app
+
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+# Init db
+db = SQLAlchemy(app)
+# Init ma... Helps with schema stuff?
+ma = Marshmallow(app)
+
+# User Class/Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    UserName = db.Column(db.String(50))
+    UserPass = db.Column(db.String(50))
+    UserDisplayName = db.Column(db.String(50))
+
+def __init__(self, UserName, UserPass, UserDisplayName):
+    self.UserName = UserName
+    self.UserPass = UserPass
+    self.UserDisplayName = UserDisplayName
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'UserName', 'UserPass', 'UserDisplayName')
+
+# Init schema
+user_schema = UserSchema()
+users_schema = UserSchema()
