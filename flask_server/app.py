6c03d15e79c189a95ad1e71cdcf087fdd491d07d
IanTaiAhn <iantaiahn@yahoo.com>
2022-10-28 00:02:03 -0600

    Okay we figured out a lot of stuff.

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,14 +1,72 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+# from flask_cors import CORS
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+# Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+# Init db
+db = SQLAlchemy(app)
+# Init ma... Helps with schema stuff?
+ma = Marshmallow(app)
+
+
+# User Class/Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    UserName = db.Column(db.String(50))
+    UserPass = db.Column(db.String(50))
+    UserDisplayName = db.Column(db.String(50))
+
+def __init__(self, UserName, UserPass, UserDisplayName):
+    self.UserName = UserName
+    self.UserPass = UserPass
+    self.UserDisplayName = UserDisplayName
+
+# User Schema
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'UserName', 'UserPass', 'UserDisplayName')
+
+# Init schema
+user_schema = UserSchema()
+users_schema = UserSchema()
+
+# API call to create a User
+@app.route('/add_user', methods=['POST'])
+def create_user():
+    UserName = request.json['UserName']
+    UserPass = request.json['UserPass']
+    UserDisplayName = request.json['UserDisplayName']
+
+    add_user = User(UserName=UserName, UserPass=UserPass, UserDisplayName=UserDisplayName)
+    db.session.add(add_user)
+    db.session.commit()
+
+    return user_schema.jsonify(add_user)
+
+# Get a User
+@app.route('/user', methods=['GET'])
+def get_users():
+    all_users = User.query.get(1)
+    result = users_schema.dump(all_users)
+    # users = db.session.execute(db.select(User).order_by(User.UserName))
+    return users_schema.jsonify(all_users)
+    # return jsonify(result)
+    
+
+# Test API Call
+@app.route('/test')
+def test():
+    return 'Testing server!'
+
 with app.app_context():
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sqlite3'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
-    db = SQLAlchemy(app)
-    class User(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        UserName = db.Column(db.String(50))
-        UserPass = db.Column(db.String(50))
-        UserDisplayName = db.Column(db.String(50))
     db.create_all()
+    
+# Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
--- flask_server/server.py
+++ flask_server/server.py
@@ -1,26 +1,23 @@
+from flask import Flask
+from flask_cors import CORS
 
+app = Flask(__name__)
+CORS(app)
 
-# Delete this line, and let the bottom of this be the top.
-# from flask import Flask
-# from flask_cors import CORS
+@app.route('/api/members')
+def members():
+    return {"members": [
+        {"name": "John", "age": 30, "score": 1000},
+        {"name": "Jane", "age": 25, "score": 2000},
+        {"name": "Bob", "age": 20, "score": 3000},
+        {"name": "Alice", "age": 15, "score": 4000},
+        {"name": "Joe", "age": 10, "score": 5000},
+        {"name": "Jill", "age": 5, "score": 6000},
+        ]}
 
-# app = Flask(__name__)
-# CORS(app)
+@app.route('/random')
+def random():
+    return {"random": 42}
 
-# @app.route('/api/members')
-# def members():
-#     return {"members": [
-#         {"name": "John", "age": 30, "score": 1000},
-#         {"name": "Jane", "age": 25, "score": 2000},
-#         {"name": "Bob", "age": 20, "score": 3000},
-#         {"name": "Alice", "age": 15, "score": 4000},
-#         {"name": "Joe", "age": 10, "score": 5000},
-#         {"name": "Jill", "age": 5, "score": 6000},
-#         ]}
-
-# @app.route('/random')
-# def random():
-#     return {"random": 42}
-
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
