c15f1b3b8c245c2762f11335734eabc02fb64e96
IanTaiAhn <iantaiahn@mail.weber.edu>
2022-12-01 23:02:29 -0700

    Tests have been added, more on the way!

--- frontend/src/Navbar/Navbar.js
+++ frontend/src/Navbar/Navbar.js
@@ -55,21 +55,7 @@ const Navbar = () => {
           <NavbarItem title="Piggies!" href="/piggy_game/piggy.html" /> */}
         </div>
 
-        {/* <!-- Hamburger menu (responsive menu) --> */}
-        <div className="relative m-2 md:hidden">
-          {/* I'll need to figure out how to make this responsive using useEffect, or useState. */}
-          <i className="fa-solid fa-bars fa-2x cursor-pointer" id="menu"></i>
-          <div className="container flex justify-center fixed top-20 -left-full w-0 h-screen transition-all ease-in duration-400 bg-pink-400 z-10">
-            <div className="flex flex-col pt-4 space-y-10 text-2xl text-white">
-              <NavbarItem title="Home" href="/" />
-              <NavbarItem title="Menu" href="/Menu" />
-              <NavbarItem title="Leaderboard" href="/Server" />
-              <NavbarItem title="HangmanReact" href="/Hangman" />
-              <NavbarItem title="Piggies!" href="/piggy_game/piggy.html" />
-            </div>
-          </div>
-        </div>
-        {/* Responsive menu end... */}
+        {/* If I want to make it responsive I will. */}
       </div>
     </nav>
     // </section>
