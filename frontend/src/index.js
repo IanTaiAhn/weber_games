96dbc4412986aad88f2060f06c4f276592b0a38e
IanTaiAhn <iantaiahn@mail.weber.edu>
2022-12-03 10:26:02 -0700

    Change in browser router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -13,7 +13,7 @@ import Footer from "./Footer/Footer";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="{process.env.PUBLIC_URL}">
       <Navbar />
       <App />
       <Footer />
